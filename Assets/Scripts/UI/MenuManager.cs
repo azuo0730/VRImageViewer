ca9599bbf1e8b48452fcb84cd5aaee77cde8039a
azuo0730 <azuo0730@gmail.com>
2018-09-06 17:54:06 +0900

    test

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	[SerializeField]
+	GameObject					FileContentPrefab = null;
+	[SerializeField]
+	GameObject					DirectoryContentPrefab = null;
+
+	[SerializeField]
+	Transform					FileContentTargetTransform = null;
+
+	// Use this for initialization
+	void Start () {
+		
+		var obj = Instantiate(FileContentPrefab, FileContentTargetTransform);
+		var obj2 = Instantiate(DirectoryContentPrefab, FileContentTargetTransform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
