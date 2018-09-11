ff9a8051c976a62e2ddb3162e43178356b482f5a
azuo0730 <azuo0730@gmail.com>
2018-09-11 15:36:44 +0900

    Oculusライブラリ追加。 フォルダ構成見直し。 アス比変更に対して、レイアウトも自動調整するように。

---
+++ Assets/VRViewer/Scripts/UI/ImageObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageObject : MonoBehaviour
+{
+	[SerializeField]
+	MeshRenderer						meshRenderer = null;
+	[SerializeField]
+	Transform							scaleTransform = null;
+
+
+	/// <summary>
+	/// 画像オブジェクトを初期化する
+	/// </summary>
+	/// <param name="tex"></param>
+	public void Init(Texture2D tex, float height)
+	{
+		meshRenderer.material.mainTexture = tex;
+
+		float aspectRatio = (float)tex.width / (float)tex.height;
+
+		// アス比を調整
+		var size = new Vector3(height*aspectRatio, height, 1.0f);
+		scaleTransform.localScale = size;
+
+		var layoutElement = GetComponent<LayoutElement>();
+		if( CommonUtility.CheckNull(layoutElement) ) return;
+		layoutElement.preferredWidth = size.x;
+		layoutElement.preferredHeight = size.y;
+	}
+
+}
